0e8ded26925426f25212189a6c9bfcf0c03ee95b
Anton <antonlopatkin62@gmail.com>
2025-09-05 17:50:27 +0900

    Perfect_read_me_file_and_code_transparency

--- src/main.cpp
+++ src/main.cpp
@@ -3,30 +3,33 @@
 #define LED_PIN_2 3
 #define LED_PIN_3 4
 
-const int ledPins[] = {LED_PIN_1, LED_PIN_2, LED_PIN_3};  // Используем реальные номера пинов
+const int ledPins[] = {LED_PIN_1, LED_PIN_2, LED_PIN_3};
 const int pinCount = sizeof(ledPins) / sizeof(ledPins[0]);
+
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   for(int i = 0; i < pinCount; i++) {
     pinMode(ledPins[i], OUTPUT);
-    digitalWrite(ledPins[i], LOW);  // Гасим все светодиоды при старте
+    digitalWrite(ledPins[i], LOW);
   }
 }
+
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Прямая последовательность
   for(int i = 0; i < pinCount; i++) {
     digitalWrite(ledPins[i], 1);
     delay(1000);
     digitalWrite(ledPins[i], 0);
     Serial.print("Activating pin: ");
-	Serial.println(ledPins[i]);
+    Serial.println(ledPins[i]);
   }
+  
+  // Обратная последовательность
   for(int i = 2; i > -1; i--) {
     digitalWrite(ledPins[i], 1);
     delay(1000);
     digitalWrite(ledPins[i], 0);
     Serial.print("Activating pin: ");
-	Serial.println(ledPins[i]);
+    Serial.println(ledPins[i]);
   }
 }
